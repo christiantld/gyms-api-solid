bde6b02ca83f72ea1cba8533afa3e0339fab4024
Christian Toledo <chrstiantoledo@gmail.com>
2024-06-13 17:54:47 -0300

    feat: register middlewares and routes on app instance

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,28 @@
 import fastify from 'fastify'
-import { appRoutes } from './http/routes'
+import { userRoutes } from './http/controllers/users/routes'
 import { ZodError } from 'zod'
 import { env } from './env'
+import fastifyJwt from '@fastify/jwt'
+import fastifyCookie from '@fastify/cookie'
+import { gymRoutes } from './http/controllers/gyms/routes'
+import { checkInsRoutes } from './http/controllers/check-ins/routes'
 
 export const app = fastify()
+app.register(fastifyCookie)
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: 'refreshToken',
+    signed: true,
+  },
+  sign: {
+    expiresIn: '10m',
+  },
+})
 
-app.register(appRoutes)
+app.register(userRoutes)
+app.register(gymRoutes)
+app.register(checkInsRoutes)
 
 app.setErrorHandler((error, _, reply) => {
   if (error instanceof ZodError) {
