d59922a932dc45d60b7cb3d0bc11568065087196
Christian Toledo <chrstiantoledo@gmail.com>
2024-06-13 17:32:58 -0300

    fix: delete unused route files

--- src/http/routes.ts
+++
@@ -1,6 +0,0 @@
-import { FastifyInstance } from 'fastify'
-import { registerUserController } from './controllers/registerUser'
-
-export async function appRoutes(app: FastifyInstance) {
-  app.post('/users', registerUserController)
-}
